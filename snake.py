64da91b5ea0a170c55ed26f4da83e077cf4dbb84
Tch1b0 <Jpour3006@gmail.com>
2021-04-09 11:46:57 +0200

    Snake: set threaded to True in app.run

--- snake.py
+++ snake.py
@@ -35,4 +35,4 @@ def end():
     return HTTPStatus.OK
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True, threaded=True)

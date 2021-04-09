2cd7880660679fcde2aa0d9b7f3e5f2b31db462b
Tch1b0 <Jpour3006@gmail.com>
2021-04-09 11:00:41 +0200

    Corrected routes

--- snake.py
+++ snake.py
@@ -8,23 +8,24 @@ def snake():
     data = {
         "apiversion": 1,
         "author": "Tch1b0",
-        "color": "#888888",
+        "color": "#400080",
         "head": "caffeine",
         "tail": "coffee",
         "version": "0.0.1"
     }
-    return jsonify(data)
+    return jsonify(data), HTTPStatus.OK
 
 @app.route("/start")
 def start():
     return jsonify({"status": "ok"}), HTTPStatus.OK
 
-@app.route("move")
+@app.route("/move")
 def move():
-    return jsonify({"move": "up"})
+    return jsonify({"move": "up"}), HTTPStatus.OK
 
-@app.route("end")
+@app.route("/end")
 def end():
     return jsonify({"status": "ok"}), HTTPStatus.OK
 
-app.run("localhost", 3000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)

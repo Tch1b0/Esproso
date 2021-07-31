6e16185345d320850f7b7e761df1ce788b6fddf0
Tch1b0 <Jpour3006@gmail.com>
2021-07-31 17:58:04 +0200

    debugging

--- brain/util.go
+++ brain/util.go
@@ -1,6 +1,10 @@
 package brain
 
-import GameData "github.com/Tch1b0/Esproso/data"
+import (
+	"fmt"
+
+	GameData "github.com/Tch1b0/Esproso/data")
+
 
 func containsString(arr []string, val string) bool {
 	for _, a := range arr {
@@ -21,6 +25,7 @@ func containsCoordinate(arr []*GameData.Coordinate, val GameData.Coordinate) boo
 }
 
 func getDirection(pos1 GameData.Coordinate, pos2 GameData.Coordinate) string {
+	fmt.Println("GET DIRECTION")
 	if pos1.Y < pos2.Y {
 		return "up"
 	}

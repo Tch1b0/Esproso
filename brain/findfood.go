c5fea42856c9ba29b5a8a991e3e5e57116ad89b3
tch1b0 <jpour3006@gmail.com>
2021-12-19 10:14:51 +0100

    fix some minor issues

--- brain/findfood.go
+++ brain/findfood.go
@@ -6,18 +6,17 @@ import (
 	GameData "github.com/Tch1b0/Esproso/data"
 )
 
-
 func findFood(data GameData.Data) GameData.Coordinate {
 	position := data.You.Head
-	var lowestDistance float64 = 1000
-	var closestFood GameData.Coordinate 
-	
+	var lowestDistance float64 = -1
+	var closestFood GameData.Coordinate
+
 	for _, food := range data.Board.Food {
 		distY := position.Y - food.Y
 		distX := position.X - food.X
-		distance := math.Sqrt(math.Pow(float64(distY), 2) * math.Pow(float64(distX), 2)) 
+		distance := math.Sqrt(math.Pow(float64(distY), 2) * math.Pow(float64(distX), 2))
 
-		if distance < lowestDistance {
+		if distance < lowestDistance || lowestDistance == -1 {
 			lowestDistance = distance
 			closestFood = food
 		}

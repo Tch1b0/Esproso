89bfc438650d700c2dea74ad87005ce63063134e
Tch1b0 <Jpour3006@gmail.com>
2022-01-21 16:34:49 +0100

    enhance testing

--- main_test.go
+++ main_test.go
@@ -10,18 +10,24 @@ import (
 	GameData "github.com/Tch1b0/Esproso/data"
 )
 
-func TestMoves(t *testing.T) {
+type SampleRequest struct {
+	Data             GameData.Data `json:"data"`
+	ExpectedResponse string        `json:"expect"`
+}
+
+func TestBrain(t *testing.T) {
 	text, _ := ioutil.ReadFile("./samples/sample_requests.json")
-	data := []GameData.Data{}
-	if err := json.Unmarshal(text, &data); err != nil {
+	samples := []SampleRequest{}
+	if err := json.Unmarshal(text, &samples); err != nil {
 		fmt.Print(err)
 		return
 	}
 
-	t.Run("Test Move", func(t *testing.T) {
-		expected := "up"
-		if ans := brain.Move(data[0]); ans != "up" {
-			t.Errorf("Expected move \"%s\", but received \"%s\"", expected, ans)
-		}
-	})
+	for _, sample := range samples {
+		t.Run("Test Move", func(t *testing.T) {
+			if ans := brain.Move(sample.Data); ans != sample.ExpectedResponse {
+				t.Errorf("Expected move \"%s\", but received \"%s\"", sample.ExpectedResponse, ans)
+			}
+		})
+	}
 }
